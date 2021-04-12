e201fa3eb6e0219c83eb4b5f81f3a5fa0d88edd7
Pedro Wendelstein Xavier <pedro.xavier@dbserver.com.br>
2021-04-12 17:33:08 -0300

    fourth story finished

---
+++ ProjetoFinal/Features/StoreSearchBar.feature
@@ -0,0 +1,21 @@
+﻿Feature: StoreSearchBar
+	Like a client I'd like to search for my products by a search bar.
+
+@mytag
+Scenario: To search with success
+	Given That I am a client on the Home page
+	When I search by "Dress" in the store search bar
+	Then I will be redirected to the Search page
+	And A list of products should be displayed on the Search page
+
+Scenario: To search with empty search bar
+	Given That I am a client on the Home page
+	When I search by "" in the store search bar
+	Then I will be redirected to the Search page
+	And The message "Please enter a search keyword" should be displayed on the Search page
+
+Scenario: To search for a product that does not exist
+	Given That I am a client on the Home page
+	When I search by "Pasta" in the store search bar
+	Then I will be redirected to the Search page
+	And The message "No results were found for your search" should be displayed on the Search page
---
+++ ProjetoFinal/StepDefinitions/StoreSearchBarSteps.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjetoFinal.PageObjects;
+using ProjetoFinal.TestContext;
+using System;
+using TechTalk.SpecFlow;
+
+namespace ProjetoFinal.StepDefinitions
+{
+    [Binding]
+    public class StoreSearchBarSteps
+    {
+        private Context _context;
+        private HomePO _homePage;
+        private SearchPO _searchPage;
+
+        public StoreSearchBarSteps(Context context)
+        {
+            _context = context;
+            _homePage = new HomePO(_context.Driver);
+            _searchPage = new SearchPO(_context.Driver);
+        }
+
+        [Given(@"That I am a client on the Home page")]
+        public void GivenThatIAmAClientOnTheHomePage()
+        {
+            _homePage.ToGoToHomePageByUrl();
+        }
+        
+        [When(@"I search by ""(.*)"" in the store search bar")]
+        public void WhenISearchByInTheStoreSearchBar(string p0)
+        {
+            _homePage.ToSearchByTheSearchBox(p0);
+        }
+        
+        [Then(@"I will be redirected to the Search page")]
+        public void ThenIWillBeRedirectedToTheSearchPage()
+        {
+            StringAssert.Contains(_context.Driver.Url.ToLower(), "controller=search".ToLower());
+        }
+        
+        [Then(@"A list of products should be displayed on the Search page")]
+        public void ThenAListOfProductsShouldBeDisplayedOnTheSearchPage()
+        {
+            int numberOfProducts = _searchPage.NumberOfSearchResults();
+            Assert.IsTrue( numberOfProducts > 0, "There are no products on page.");
+        }
+        
+        [Then(@"The message ""(.*)"" should be displayed on the Search page")]
+        public void ThenTheMessageShouldBeDisplayedOnTheSearchPage(string p0)
+        {
+            StringAssert.Contains(_context.Driver.PageSource.ToLower(), p0.ToLower());
+        }
+    }
+}
